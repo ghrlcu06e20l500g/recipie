6783b414600e9b84d92505e76353aa73d18a22a1
Luca Ghironzi <148538702+ghrlcu06e20l500g@users.noreply.github.com>
2025-04-29 08:46:34 +0200

    Fixed previous bug for general files.

---
+++ connection.php
@@ -0,0 +1,30 @@
+<?php
+
+function query($query, $types = null, ...$_): array {
+    $connection = new mysqli("localhost", "recipie", "", "my_recipie");
+    if($connection -> connect_error) {
+        die("Failed to establish connection.");
+    }
+    $statement = $connection -> prepare($query);
+    if(!$statement) {
+        die("Failed to create statement.");
+    }
+    if($types != null) {
+        $statement -> bind_param($types, ...$_);
+    }
+    $statement -> execute();
+    $result = $statement -> get_result();
+    if(!$result) {
+        return [];
+    }
+
+    $value = $result -> fetch_all(MYSQLI_ASSOC) ?? [];
+
+    $result -> close();
+    $statement -> close();
+    $connection -> close();
+
+    return $value;
+}
+
+?>
---
+++ utils.php
@@ -0,0 +1,47 @@
+<?php
+
+function suffix(string $measurementType): string {
+	switch($measurementType) {
+        case "QUANTITY": return "";
+        case "WEIGHT": return "g";
+        case "VOLUME": return "ml";
+        default:
+      	    throw new Exception($measurementType." is an invalid measurement type.");
+    };
+}
+function loadIngredients(): array | null {
+    require "/membri/recipie/views/connect.php";
+
+    $statement = $connection -> prepare("
+    	SELECT id, name
+        FROM Ingredient
+        ORDER BY name
+    ");
+    $statement -> execute();
+    $result = $statement -> get_result();
+    
+    if($result -> num_rows == 0) {
+        return null;
+    } else {
+        return $result -> fetch_all(MYSQLI_ASSOC);
+    }
+}
+function loadAllergens(): array | null {
+    require "/membri/recipie/views/connect.php";
+
+    $statement = $connection -> prepare("
+    	SELECT id, name
+        FROM Allergen
+        ORDER BY name
+    ");
+    $statement -> execute();
+    $result = $statement -> get_result();
+    
+    if($result -> num_rows == 0) {
+        return null;
+    } else {
+        return $result -> fetch_all(MYSQLI_ASSOC);
+    }
+}
+
+?>
